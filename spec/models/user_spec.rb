00eb2c6e3999c271b6e4a8be3fb1495d16ed422a
Christopher Kelling <cj.kelling88@gmail.com>
2019-12-19 12:26:43 -0700

    Add uid validations to the model test for Users
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_presence_of :token }
+    it { should validate_presence_of :uid }
 
     it { should allow_value('user@example.com').for(:email) }
     it { should_not allow_value('user.example.com').for(:email) }

adefcf4aaabe695e286ab17a690fdf533dc85999
John Travers <johnk.travers@gmail.com>
2019-12-19 14:00:25 -0700

    Add route to help page
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get '/register', to: 'users#new'
+  get '/register',  to: 'users#new'
+  get '/help',      to: 'help#index'
   get '/dashboard', to: 'users#show'
 
   get '/auth/google_oauth2', as: 'google_connect'

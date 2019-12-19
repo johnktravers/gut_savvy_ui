57c32f9408a87878c7fbd14e2bea4aafab528586
Christopher Kelling <cj.kelling88@gmail.com>
2019-12-19 12:26:22 -0700

    Add feature test to validate a user can login with Google OAuth
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

---
+++ spec/features/registration_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  describe 'When I visit the registration page' do
+    it 'I can connect to my Google account and register for the application' do
+      visit register_path
+
+      click_link 'Create an Account using Google'
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content('Your account has been created using Google!')
+    end
+  end
+end

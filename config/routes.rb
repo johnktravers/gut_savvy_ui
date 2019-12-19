9baa0b99d3ba8038e630970b76fdf2b9b5e3b95f
Christopher Kelling <cj.kelling88@gmail.com>
2019-12-19 12:27:47 -0700

    Add user registration and dashboard routes. Add OmniAuth routes to connect with Google and receive a callback.
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
+  get '/register', to: 'users#new'
+  get '/dashboard', to: 'users#show'
+
+  get '/auth/google_oauth2', as: 'google_connect'
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
 end

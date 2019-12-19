5db160f6aa5013999d0a3aa26340ea149e636470
John Travers <johnk.travers@gmail.com>
2019-12-18 19:15:55 -0700

    Update database schema with user, meal, dish, food, ingredient, and joins tables
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

---
+++ db/schema.rb
@@ -0,0 +1,99 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_12_19_020545) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "dish_foods", force: :cascade do |t|
+    t.bigint "dish_id"
+    t.bigint "food_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dish_id"], name: "index_dish_foods_on_dish_id"
+    t.index ["food_id"], name: "index_dish_foods_on_food_id"
+  end
+
+  create_table "dishes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "food_ingredients", force: :cascade do |t|
+    t.bigint "food_id"
+    t.bigint "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_food_ingredients_on_food_id"
+    t.index ["ingredient_id"], name: "index_food_ingredients_on_ingredient_id"
+  end
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "upc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "meal_dishes", force: :cascade do |t|
+    t.bigint "meal_id"
+    t.bigint "dish_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dish_id"], name: "index_meal_dishes_on_dish_id"
+    t.index ["meal_id"], name: "index_meal_dishes_on_meal_id"
+  end
+
+  create_table "meal_ingredients", force: :cascade do |t|
+    t.bigint "meal_id"
+    t.bigint "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_meal_ingredients_on_ingredient_id"
+    t.index ["meal_id"], name: "index_meal_ingredients_on_meal_id"
+  end
+
+  create_table "meals", force: :cascade do |t|
+    t.string "title"
+    t.integer "gut_feeling"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_meals_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "dish_foods", "dishes"
+  add_foreign_key "dish_foods", "foods"
+  add_foreign_key "food_ingredients", "foods"
+  add_foreign_key "food_ingredients", "ingredients"
+  add_foreign_key "meal_dishes", "dishes"
+  add_foreign_key "meal_dishes", "meals"
+  add_foreign_key "meal_ingredients", "ingredients"
+  add_foreign_key "meal_ingredients", "meals"
+  add_foreign_key "meals", "users"
+end

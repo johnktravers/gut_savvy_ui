3052099fe60a15f41ffedce2a47e13c9b6528713
Jon Patterson <JonPatt92@gmail.com>
2019-12-19 13:50:45 -0700

    Add routes for results index page and meals new page
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get '/register', to: 'users#new'
-  get '/dashboard', to: 'users#show'
+  get '/register',                    to: 'users#new'
+  get '/dashboard',                   to: 'users#show'
+  
+  get '/results',                     to: 'results#index'
 
-  get '/auth/google_oauth2', as: 'google_connect'
+  get '/auth/google_oauth2',          as: 'google_connect'
   get '/auth/google_oauth2/callback', to: 'sessions#create'
+
+  resources :meals,                 only: [:new]
 end

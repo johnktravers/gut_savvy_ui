fc37cc8f0dadf758edf13e77d129a536d861c147
Jon Patterson <JonPatt92@gmail.com>
2019-12-20 09:01:58 -0700

    Adjust tabbing
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  get '/help', to: 'help#show'
-  get '/sign_in', to: 'users#new'
+  get '/help',                        to: 'help#show'
+  get '/sign_in',                     to: 'users#new'
   get '/dashboard',                   to: 'users#show'
 
   get '/results',                     to: 'results#index'
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   get '/auth/google_oauth2/callback', to: 'sessions#create'
   delete '/logout',                   to: 'sessions#destroy'
 
-  resources :meals, only: [:new]
+  resources :meals,                 only: [:new]
 end

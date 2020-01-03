f09625a205ae7038fa1cd0831574685b1dec3c08
Ryan Hantak <rhantak@gmail.com>
2020-01-02 17:01:30 -0700

    Remove validation of gut feeling on meals
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -3,11 +3,6 @@ require 'rails_helper'
 RSpec.describe Meal, type: :model do
   describe 'validations' do
     it { should validate_presence_of :title }
-    it { should validate_presence_of :gut_feeling }
-
-    it { should validate_numericality_of(:gut_feeling).only_integer }
-    it { should validate_numericality_of(:gut_feeling).is_greater_than_or_equal_to(-5) }
-    it { should validate_numericality_of(:gut_feeling).is_less_than_or_equal_to(5) }
   end
 
   describe 'relationships' do

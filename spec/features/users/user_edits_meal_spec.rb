65d8a5a6a8d0d44187f4a7a6999f4b96c5e04899
Jon Patterson <JonPatt92@gmail.com>
2020-01-03 14:37:36 -0700

    Add test for -5 to 5 mandatory gut feeling range

--- spec/features/users/user_edits_meal_spec.rb
+++ spec/features/users/user_edits_meal_spec.rb
@@ -17,7 +17,12 @@ RSpec.describe 'As a user' do
       fill_in 'meal[gut_feeling]', with: -3
       click_button 'Update'
 
+      unrated_meal.reload
+      expect(unrated_meal.gut_feeling).to eq(-3)
+
       expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("You have successfully recorded your gut feeling for #{unrated_meal.title}")
+
       within "#meal-#{unrated_meal.id}" do
         expect(page).to have_content(-3)
       end
@@ -29,15 +34,38 @@ RSpec.describe 'As a user' do
       visit edit_meal_path(rated_meal)
       expect(page).to have_content(rated_meal.title)
 
-      expect(find_field('meal[gut_feeling]').value).to eq(1)
+      expect(find_field('meal[gut_feeling]').value).to eq('1')
 
       fill_in 'meal[gut_feeling]', with: -3
       click_button 'Update'
 
+      rated_meal.reload
+      expect(rated_meal.gut_feeling).to eq(-3)
+
       expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("You have successfully recorded your gut feeling for #{rated_meal.title}")
+
       within "#meal-#{rated_meal.id}" do
         expect(page).to have_content(-3)
       end
+    end
+
+    it 'I cannot use a gut feeling outside of -5 to 5 range' do
+      unrated_meal = create(:unrated_meal, user: @user)
+
+      visit edit_meal_path(unrated_meal)
+
+      fill_in 'meal[gut_feeling]', with: -6
+      click_button 'Update'
+
+      expect(current_path).to_not eq(dashboard_path)
+      expect(page).to have_content("Gut feeling must be greater than or equal to -5")
+
+      fill_in 'meal[gut_feeling]', with: 6
+      click_button 'Update'
+
+      expect(current_path).to_not eq(dashboard_path)
+      expect(page).to have_content("Gut feeling must be less than or equal to 5")
     end
   end
 end

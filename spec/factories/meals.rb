21cdfc12f6b67859d8baa76ad2c8c55ef1031a92
Jon Patterson <JonPatt92@gmail.com>
2020-01-03 12:38:30 -0700

    Add faker for meal title and add unrated_meal factory

--- spec/factories/meals.rb
+++ spec/factories/meals.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :meal do
-    title { 'Breakfast' }
+    title { Faker::Food.dish }
     gut_feeling { Faker::Number.between(from: -5, to: 5) }
+    user
+  end
+
+  factory :unrated_meal, class: 'Meal' do
+    title { Faker::Food.dish }
     user
   end
 end

43833c9ddcc00016adb388a6e6e864021e1170a0
Ryan Hantak <rhantak@gmail.com>
2020-01-02 17:08:34 -0700

    Add controller action and relevant instance methods to create a food
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,46 @@
+class FoodsController < ApplicationController
+  before_action :require_food_session, :require_login
+
+  def new; end
+
+  def create
+    unless food = Food.find_by(upc: params[:food][:upc])
+      service = FDCService.new
+      food_info = service.food_info(params[:food][:upc])
+      if food_info
+        food_success(food_info)
+      else
+        food_error
+      end
+    else
+      session[:foods] << food.id
+      redirect_to new_dish_path
+    end
+  end
+
+  private
+
+  def require_food_session
+    render file: "/public/404" if session[:foods].nil?
+  end
+
+  def create_food(food_info)
+    Food.create(
+      name: food_info[:description],
+      brand: food_info[:brandOwner],
+      upc: food_info[:gtinUpc]
+    )
+  end
+
+  def food_success(food_info)
+    food = create_food(food_info)
+    food.create_ingredients(food_info)
+    session[:foods] << food.id
+    redirect_to new_dish_path
+  end
+
+  def food_error
+    flash[:error] = "Please enter a valid 12-digit UPC."
+    redirect_to new_food_path
+  end
+end

dc0ebe5d5f63e1433b226093e14e6128fd8d07de
Ryan Hantak <rhantak@gmail.com>
2020-01-02 17:00:39 -0700

    Place omniauth setup in rails helper
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- spec/features/users/google_oauth_spec.rb
+++ spec/features/users/google_oauth_spec.rb
@@ -2,28 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor' do
   before :each do
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
-      'provider' => 'google_oauth2',
-      'uid' => '123456789',
-      'info' => {
-        'name' => 'John Doe',
-        'email' => 'john.doe@example.com',
-        'first_name' => 'John',
-        'last_name' => 'Doe',
-        'image' => 'https://lh3.googleusercontent.com/url/photo.jpg'
-      },
-      'credentials' => {
-        'token' => 'token',
-        'refresh_token' => 'another_token',
-        'expires_at' => 1_354_920_555,
-        'expires' => true
-      }
-    )
-
-    Rails
-      .application
-      .env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+    omniauth_setup
 
     visit sign_in_path
     click_link 'Sign in with Google'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,4 +74,29 @@ RSpec.configure do |config|
       with.library :rails
     end
   end
+
+  def omniauth_setup
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+      'provider' => 'google_oauth2',
+      'uid' => '123456789',
+      'info' => {
+        'name' => 'John Doe',
+        'email' => 'john.doe@example.com',
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+        'image' => 'https://lh3.googleusercontent.com/url/photo.jpg'
+      },
+      'credentials' => {
+        'token' => 'token',
+        'refresh_token' => 'another_token',
+        'expires_at' => 1_354_920_555,
+        'expires' => true
+      }
+    )
+
+    Rails
+      .application
+      .env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+  end
 end

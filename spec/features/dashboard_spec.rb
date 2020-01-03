d0d05672ff1c8d286a6ee03adb888fc330b87815
Ryan Hantak <rhantak@gmail.com>
2020-01-02 17:02:34 -0700

    Add test for visitor navigating to dashboard
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -25,3 +25,13 @@ describe 'As a user' do
     end
   end
 end
+
+describe 'As a visitor' do
+  describe 'when I visit the dashboard page' do
+    it 'I receive a 404 error' do
+      visit dashboard_path
+
+      expect(page).to have_content("The page you were looking for doesn't exist")
+    end
+  end
+end

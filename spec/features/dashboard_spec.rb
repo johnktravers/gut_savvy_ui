b32592d59cafe110757c8684b7aa9818cb5b768f
Jon Patterson <JonPatt92@gmail.com>
2020-01-03 12:38:52 -0700

    Build tests for adding and updating gut feelings from the dashboard

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,27 +1,90 @@
 require 'rails_helper'
 
-describe 'As a user' do
+RSpec.describe 'As a user' do
   describe 'when I visit the dashboard page' do
     before(:each) do
       @user = create(:user)
       allow_any_instance_of(ApplicationController)
       .to receive(:current_user).and_return(@user)
+
+      @unrated_meal = create(:unrated_meal, user: @user)
+
+      visit dashboard_path
     end
 
     it 'I can log a meal' do
-      visit dashboard_path
-
       click_link 'Log a Meal'
 
       expect(current_path).to eq(new_meal_path)
     end
 
-    it 'I can view my results' do
-      visit dashboard_path
-
+    it 'I can use a link to view my results' do
       click_link 'See My Results'
 
       expect(current_path).to eq(results_path)
+    end
+
+    it 'I can see a table of my previous meals and assign a gut feeling for 72 hours after meal creation' do
+      day_old_meal       = create(:unrated_meal,
+                                  user: @user,
+                                  created_at: DateTime.now.prev_day
+                                  )
+      three_day_old_meal = create(:unrated_meal,
+                                  user: @user,
+                                  created_at: DateTime.now.prev_day(3)
+                                  )
+      rated_meal         = create(:meal, user: @user)
+
+      within "#meal-#{rated_meal.id}" do
+        expect(page).to have_content(rated_meal.title)
+        expect(page).to have_content(rated_meal.gut_feeling)
+        expect(page).to have_content(rated_meal.created_at.localtime.to_s)
+        expect(page).to_not have_link('Add Gut Feeling')
+      end
+
+      within "#meal-#{three_day_old_meal.id}" do
+        expect(page).to_not have_link('Add Gut Feeling')
+      end
+
+      within "#meal-#{@unrated_meal.id}" do
+        expect(page).to have_content(@unrated_meal.title)
+        expect(page).to have_content(@unrated_meal.created_at.localtime.to_s)
+        expect(page).to have_link('Add Gut Feeling')
+      end
+
+      within "#meal-#{day_old_meal.id}" do
+        expect(page).to have_link('Add Gut Feeling')
+        click_link 'Add Gut Feeling'
+      end
+
+      expect(current_path).to eq(edit_meal_path(day_old_meal))
+    end
+
+    it 'I can update my gut feelings up to 24 hours after gut feeling creation' do
+      day_old_meal = create(:meal,
+                            user: @user,
+                            created_at: DateTime.now.prev_day(2),
+                            updated_at: DateTime.now.prev_day
+                            )
+      rated_meal   = create(:meal,
+                            user: @user,
+                            created_at: DateTime.now.prev_day(2)
+                            )
+
+      within "#meal-#{day_old_meal.id}" do
+        expect(page).to_not have_link('Update Gut Feeling')
+      end
+
+      within "#meal-#{@unrated_meal.id}" do
+        expect(page).to_not have_link('Update Gut Feeling')
+      end
+
+      within "#meal-#{rated_meal.id}" do
+        expect(page).to have_link('Update Gut Feeling')
+        click_link 'Update Gut Feeling'
+      end
+
+      expect(current_path).to eq(edit_meal_path(rated_meal))
     end
   end
 end
@@ -29,8 +92,6 @@ end
 describe 'As a visitor' do
   describe 'when I visit the dashboard page' do
     it 'I receive a 404 error' do
-      visit dashboard_path
-
       expect(page).to have_content("The page you were looking for doesn't exist")
     end
   end

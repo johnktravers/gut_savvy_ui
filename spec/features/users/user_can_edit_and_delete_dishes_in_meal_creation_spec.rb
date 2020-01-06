5cbf65b623f9036bbccc24b6de60e289539cbfbf
Ryan Hantak <rhantak@gmail.com>
2020-01-06 16:35:07 -0700

    Add test for user editing a dish

--- spec/features/users/user_can_edit_and_delete_dishes_in_meal_creation_spec.rb
+++ spec/features/users/user_can_edit_and_delete_dishes_in_meal_creation_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'As a user' do
   describe 'When I visit the meal creation page with dishes in my current meal' do
-    it "I can remove a dish from the meal I am building" do
+    before(:each) do
       omniauth_setup
       visit sign_in_path
       click_link 'Sign in with Google'
       @user = User.last
+    end
 
+    it "I can remove a dish from the meal I am building" do
       fi_1 = create(:food_ingredient)
       fi_2 = create(:food_ingredient)
       dish_1 = create(:dish)
@@ -35,6 +37,44 @@ RSpec.describe 'As a user' do
 
       within('.current-dishes') do
         expect(page).to have_content("There are currently no dishes in this meal")
+      end
+    end
+
+    it 'I can click the edit button to go to a prefilled dish creation page' do
+      visit '/dashboard'
+      click_link 'Log a Meal'
+
+      click_link "Add a New Dish"
+      click_link "Add a New Food"
+      fill_in 'food[upc]', with: "041129077122"
+      click_button "Add Food"
+      click_link "Add a New Food"
+      fill_in 'food[upc]', with: "078742058238"
+      click_button "Add Food"
+      fill_in 'dish[name]', with: 'Oily Sauce'
+      click_button 'Create Dish'
+
+      id = Dish.find_by(name: 'Oily Sauce').id
+
+      within ".current-dishes" do
+        within "#dish-#{id}" do
+          click_link 'Edit'
+        end
+      end
+
+      within ".current-foods" do
+        expect(page).to have_content("CLASSICO, TOMATO & BASIL PASTA SAUCE, TOMATO & BASIL, TOMATO & BASIL")
+        expect(page).to have_content("ITALIAN EXTRA VIRGIN OLIVE OIL")
+      end
+
+      click_link "Add a New Food"
+      fill_in 'food[upc]', with: "640671889988"
+      click_button "Add Food"
+      fill_in 'dish[name]', with: 'Raspberry Pasta Sauce'
+      click_button 'Create Dish'
+
+      within('.current-dishes') do
+        expect(page).to have_content("Raspberry Pasta Sauce")
       end
     end
   end

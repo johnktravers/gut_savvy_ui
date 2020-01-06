6416833aa99456136e8910e9840a0eddae41ec90
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-06 15:06:35 -0700

    Remove validation for uniqueness of name on the Dish model
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,5 @@
 class Dish < ApplicationRecord
   validates_presence_of :name
-  validates_uniqueness_of :name
 
   has_many :meal_dishes, dependent: :destroy
   has_many :meals, through: :meal_dishes

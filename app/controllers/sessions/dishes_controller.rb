eaa29464f23c64d18f2140b2308b4d55289852b1
Ryan Hantak <rhantak@gmail.com>
2020-01-06 15:54:47 -0700

    Add controller action to remove a dish id from dishes session
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>

--- app/controllers/sessions/dishes_controller.rb
+++ app/controllers/sessions/dishes_controller.rb
@@ -2,7 +2,12 @@ class Sessions::DishesController < ApplicationController
   before_action :require_login
 
   def update
-    session[:foods] << params[:food_id].to_i
+    session[:foods] << params[:food_id]
     redirect_to new_dish_path
+  end
+
+  def destroy
+    session[:dishes].delete(params[:dish_id])
+    redirect_to new_meal_path
   end
 end

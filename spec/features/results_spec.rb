80771876ce38d7a39fd2978731ac2bd6fd24da7d
Jon Patterson <JonPatt92@gmail.com>
2020-01-07 17:29:46 -0700

    Write test for food history table on results page

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -23,68 +23,103 @@ RSpec.describe 'As a user' do
     end
 
     it 'I can see my consumed ingredients, their associated food, avg gut feeling and # of times eaten' do
-      meal_1       = create(:meal, user: @user, gut_feeling: -5)
-      meal_2       = create(:meal, user: @user, gut_feeling: 2)
+      meal_1       = create(:meal, user: @user, gut_feeling: 3)
+      meal_2       = create(:meal, user: @user, gut_feeling: -5)
 
       dish_1       = create(:dish) # eaten once
       dish_2       = create(:dish) # eaten once
-
-      meal_dish_1  = create(:meal_dish, meal: meal_1, dish: dish_1)
-      meal_dish_2  = create(:meal_dish, meal: meal_2, dish: dish_2)
 
       food_1       = create(:food) # eaten once (dish_1)
       food_2       = create(:food) # eaten twice (dishes 1, 2)
       food_3       = create(:food) # eaten twice (dishes 1, 2)
       food_4       = create(:food) # eaten once (dish_2)
 
-
-      dish_food_1  = create(:dish_food, dish: dish_1, food: food_1)
-      dish_food_2  = create(:dish_food, dish: dish_1, food: food_2)
-      dish_food_3  = create(:dish_food, dish: dish_1, food: food_3)
-
-      dish_food_4  = create(:dish_food, dish: dish_2, food: food_2)
-      dish_food_5  = create(:dish_food, dish: dish_2, food: food_3)
-      dish_food_6  = create(:dish_food, dish: dish_2, food: food_4)
+      ingredient_1 = create(:ingredient) # found once (food_1), eaten 1 time, rating (3) #
+      ingredient_2 = create(:ingredient) # found twice (foods 2, 3), eaten 4 times, ratings (3, 3, -5, -5), avg -1 #
+      ingredient_3 = create(:ingredient) # found thrice (foods 2, 3, 4), eaten 5 times, ratings (3, 3, -5, -5, -5), avg -1.8 #
+      ingredient_4 = create(:ingredient) # found 4 times (foods 1, 2, 3, 4), eaten 6 times, ratings (3, 3, 3, -5, -5, -5), avg -1 #
 
 
-      ingredient_1 = create(:ingredient) # found once (food_1)
-      ingredient_2 = create(:ingredient) # found twice (foods 2, 3)
-      ingredient_3 = create(:ingredient) # found thrice (foods 2, 3, 4)
-      ingredient_4 = create(:ingredient) # found 4 times (foods 1, 2, 3, 4)
+      create(:meal_dish, meal: meal_1, dish: dish_1)
+      create(:meal_dish, meal: meal_2, dish: dish_2)
 
 
-      food_ingredient_1  = create(:food_ingredient, food: food_1, ingredient: ingredient_1)
-      food_ingredient_2  = create(:food_ingredient, food: food_1, ingredient: ingredient_4)
+      create(:dish_food, dish: dish_1, food: food_1)
+      create(:dish_food, dish: dish_1, food: food_2)
+      create(:dish_food, dish: dish_1, food: food_3)
 
-      food_ingredient_3  = create(:food_ingredient, food: food_2, ingredient: ingredient_2)
-      food_ingredient_4  = create(:food_ingredient, food: food_2, ingredient: ingredient_3)
-      food_ingredient_5  = create(:food_ingredient, food: food_2, ingredient: ingredient_4)
-
-      food_ingredient_6  = create(:food_ingredient, food: food_3, ingredient: ingredient_2)
-      food_ingredient_7  = create(:food_ingredient, food: food_3, ingredient: ingredient_3)
-      food_ingredient_8  = create(:food_ingredient, food: food_3, ingredient: ingredient_4)
-
-      food_ingredient_9  = create(:food_ingredient, food: food_4, ingredient: ingredient_3)
-      food_ingredient_10 = create(:food_ingredient, food: food_4, ingredient: ingredient_4)
+      create(:dish_food, dish: dish_2, food: food_2)
+      create(:dish_food, dish: dish_2, food: food_3)
+      create(:dish_food, dish: dish_2, food: food_4)
 
 
-      meal_ingredient_1   = create(:meal_ingredient, meal: meal_1, ingredient: ingredient_1)
-      meal_ingredient_2   = create(:meal_ingredient, meal: meal_1, ingredient: ingredient_2)
-      meal_ingredient_3   = create(:meal_ingredient, meal: meal_1, ingredient: ingredient_2)
-      meal_ingredient_4   = create(:meal_ingredient, meal: meal_1, ingredient: ingredient_3)
-      meal_ingredient_5   = create(:meal_ingredient, meal: meal_1, ingredient: ingredient_3)
-      meal_ingredient_6   = create(:meal_ingredient, meal: meal_1, ingredient: ingredient_4)
-      meal_ingredient_7   = create(:meal_ingredient, meal: meal_1, ingredient: ingredient_4)
-      meal_ingredient_8   = create(:meal_ingredient, meal: meal_1, ingredient: ingredient_4)
+      create(:food_ingredient, food: food_1, ingredient: ingredient_1)
+      create(:food_ingredient, food: food_1, ingredient: ingredient_4)
 
-      meal_ingredient_9   = create(:meal_ingredient, meal: meal_2, ingredient: ingredient_2)
-      meal_ingredient_10  = create(:meal_ingredient, meal: meal_2, ingredient: ingredient_2)
-      meal_ingredient_11  = create(:meal_ingredient, meal: meal_2, ingredient: ingredient_3)
-      meal_ingredient_12  = create(:meal_ingredient, meal: meal_2, ingredient: ingredient_2)
-      meal_ingredient_13  = create(:meal_ingredient, meal: meal_2, ingredient: ingredient_2)
-      meal_ingredient_14  = create(:meal_ingredient, meal: meal_2, ingredient: ingredient_2)
-      meal_ingredient_15  = create(:meal_ingredient, meal: meal_2, ingredient: ingredient_2)
+      create(:food_ingredient, food: food_2, ingredient: ingredient_2)
+      create(:food_ingredient, food: food_2, ingredient: ingredient_3)
+      create(:food_ingredient, food: food_2, ingredient: ingredient_4)
 
+      create(:food_ingredient, food: food_3, ingredient: ingredient_2)
+      create(:food_ingredient, food: food_3, ingredient: ingredient_3)
+      create(:food_ingredient, food: food_3, ingredient: ingredient_4)
+
+      create(:food_ingredient, food: food_4, ingredient: ingredient_3)
+      create(:food_ingredient, food: food_4, ingredient: ingredient_4)
+
+
+      create(:meal_ingredient, meal: meal_1, ingredient: ingredient_1)
+      create(:meal_ingredient, meal: meal_1, ingredient: ingredient_2)
+      create(:meal_ingredient, meal: meal_1, ingredient: ingredient_2)
+      create(:meal_ingredient, meal: meal_1, ingredient: ingredient_3)
+      create(:meal_ingredient, meal: meal_1, ingredient: ingredient_3)
+      create(:meal_ingredient, meal: meal_1, ingredient: ingredient_4)
+      create(:meal_ingredient, meal: meal_1, ingredient: ingredient_4)
+      create(:meal_ingredient, meal: meal_1, ingredient: ingredient_4)
+
+      create(:meal_ingredient, meal: meal_2, ingredient: ingredient_2)
+      create(:meal_ingredient, meal: meal_2, ingredient: ingredient_2)
+      create(:meal_ingredient, meal: meal_2, ingredient: ingredient_3)
+      create(:meal_ingredient, meal: meal_2, ingredient: ingredient_3)
+      create(:meal_ingredient, meal: meal_2, ingredient: ingredient_3)
+      create(:meal_ingredient, meal: meal_2, ingredient: ingredient_4)
+      create(:meal_ingredient, meal: meal_2, ingredient: ingredient_4)
+      create(:meal_ingredient, meal: meal_2, ingredient: ingredient_4)
+
+      visit results_path
+
+      within ".ingredient-history" do
+        within "#ingredient-#{ingredient_1.id}" do
+          expect(page).to have_content(ingredient_1.name)
+          expect(page).to have_content('3.0')
+          expect(page).to have_content('1')
+          expect(page).to have_content(food_1.name)
+        end
+        within "#ingredient-#{ingredient_2.id}" do
+          expect(page).to have_content(ingredient_2.name)
+          expect(page).to have_content('-1.0')
+          expect(page).to have_content('4')
+          expect(page).to have_content(food_2.name)
+          expect(page).to have_content(food_3.name)
+        end
+        within "#ingredient-#{ingredient_3.id}" do
+          expect(page).to have_content(ingredient_3.name)
+          expect(page).to have_content('-1.8')
+          expect(page).to have_content('5')
+          expect(page).to have_content(food_2.name)
+          expect(page).to have_content(food_3.name)
+          expect(page).to have_content(food_4.name)
+        end
+        within "#ingredient-#{ingredient_4.id}" do
+          expect(page).to have_content(ingredient_4.name)
+          expect(page).to have_content('-1.0')
+          expect(page).to have_content('6')
+          expect(page).to have_content(food_1.name)
+          expect(page).to have_content(food_2.name)
+          expect(page).to have_content(food_3.name)
+          expect(page).to have_content(food_4.name)
+        end
+      end
     end
   end
 end

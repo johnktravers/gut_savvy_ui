9db06555c0c30fd642474507357b87b6d7175d98
John Travers <johnk.travers@gmail.com>
2020-01-08 10:32:25 -0700

    Add test that gut feelings timeline appears on results page
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>

--- spec/features/users/results_page_spec.rb
+++ spec/features/users/results_page_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe 'As a registered user' do
       expect(page).to_not have_content('You need to log 9 more meals with Gut Feelings')
     end
 
-    it "I see bar graphs for my best and worst ingredients based on gut feeling" do
+    it "I see bar graphs for my best and worst ingredients based on gut feeling", js: true do
       user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -61,7 +61,31 @@ RSpec.describe 'As a registered user' do
         expect(page).to have_css('svg')
       end
 
-      save_and_open_page
+    end
+  end
+
+  it "I see a line graph of my gut feelings over time" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    create(:meal, user: user, gut_feeling: -5, created_at: '2019-12-21 07:54:09')
+    create(:meal, user: user, gut_feeling: -4, created_at: '2019-12-21 14:54:09')
+    create(:meal, user: user, gut_feeling: -3, created_at: '2019-12-21 19:54:09')
+    create(:meal, user: user, gut_feeling: -2, created_at: '2019-12-23 07:54:09')
+    create(:meal, user: user, gut_feeling: -1, created_at: '2019-12-23 19:54:09')
+    create(:meal, user: user, gut_feeling: 0,  created_at: '2019-12-24 07:54:09')
+    create(:meal, user: user, gut_feeling: 1,  created_at: '2019-12-24 12:54:09')
+    create(:meal, user: user, gut_feeling: 1,  created_at: '2019-12-24 19:54:09')
+    create(:meal, user: user, gut_feeling: 2,  created_at: '2019-12-25 11:54:09')
+    create(:meal, user: user, gut_feeling: 3,  created_at: '2019-12-25 15:54:09')
+    create(:meal, user: user, gut_feeling: 4,  created_at: '2019-12-26 10:54:09')
+    create(:meal, user: user, gut_feeling: 5,  created_at: '2019-12-27 14:54:09')
+    create(:meal, user: user, gut_feeling: 2,  created_at: '2019-12-27 19:54:09')
+
+    visit results_path
+
+    within '#gut-feeling-timeline' do
+      expect(page).to have_css('svg')
     end
   end
 end

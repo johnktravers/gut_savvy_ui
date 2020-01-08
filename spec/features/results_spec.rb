636d86ddbceb05241af6e6318b9a89e57bd6d6c1
Jon Patterson <JonPatt92@gmail.com>
2020-01-07 17:58:31 -0700

    Change within class to represent unique value

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'As a user' do
 
     it 'I can visit my results page from the landing page' do
       visit '/'
-      within ".nav-item" do
+      within ".navbar-nav" do
         click_link 'Results'
       end
 

f8d5b8be2518ec4e148aab93ed8fce06eb948a7e
Jon Patterson <JonPatt92@gmail.com>
2020-01-08 12:17:52 -0700

    Add test for all_foods instance method

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -26,6 +26,25 @@ RSpec.describe Ingredient, type: :model do
       @ingredient_3 = create(:ingredient) # found thrice (foods 2, 3, 4), eaten 5 times, ratings (3, 3, -5, -5, -5), avg -1.8 #
       @ingredient_4 = create(:ingredient) # found 4 times (foods 1, 2, 3, 4), eaten 6 times, ratings (3, 3, 3, -5, -5, -5), avg -1 #
 
+      @food_1       = create(:food) # eaten once (dish_1)
+      @food_2       = create(:food) # eaten twice (dishes 1, 2)
+      @food_3       = create(:food) # eaten twice (dishes 1, 2)
+      @food_4       = create(:food) # eaten once (dish_2)
+
+      create(:food_ingredient, food: @food_1, ingredient: @ingredient_1)
+      create(:food_ingredient, food: @food_1, ingredient: @ingredient_4)
+
+      create(:food_ingredient, food: @food_2, ingredient: @ingredient_2)
+      create(:food_ingredient, food: @food_2, ingredient: @ingredient_3)
+      create(:food_ingredient, food: @food_2, ingredient: @ingredient_4)
+
+      create(:food_ingredient, food: @food_3, ingredient: @ingredient_2)
+      create(:food_ingredient, food: @food_3, ingredient: @ingredient_3)
+      create(:food_ingredient, food: @food_3, ingredient: @ingredient_4)
+
+      create(:food_ingredient, food: @food_4, ingredient: @ingredient_3)
+      create(:food_ingredient, food: @food_4, ingredient: @ingredient_4)
+
       create(:meal_ingredient, meal: @meal_1, ingredient: @ingredient_1)
       create_list(:meal_ingredient, 2, meal: @meal_1, ingredient: @ingredient_2)
       create_list(:meal_ingredient, 2, meal: @meal_1, ingredient: @ingredient_3)
@@ -43,11 +62,23 @@ RSpec.describe Ingredient, type: :model do
       expect(@ingredient_4.times_eaten(@user)).to eq(6)
     end
 
-    it 'can return average gut feeling across all meals' do
+    it 'can return average gut feeling for ingredient across all meals' do
       expect(@ingredient_1.average_gut_feeling(@user)).to eq(3)
       expect(@ingredient_2.average_gut_feeling(@user)).to eq(-1)
       expect(@ingredient_3.average_gut_feeling(@user)).to eq(-1.8)
       expect(@ingredient_4.average_gut_feeling(@user)).to eq(-1)
+    end
+
+    it 'can return all foods consumed by user which contain ingredient' do
+      expected_1 = [@food_1]
+      expected_2 = [@food_2, @food_3]
+      expected_3 = [@food_2, @food_3, @food_4]
+      expected_4 = [@food_1, @food_2, @food_3, @food_4]
+
+      expect(@ingredient_1.all_foods(@user)).to eq(expected_1)
+      expect(@ingredient_2.all_foods(@user)).to eq(expected_2)
+      expect(@ingredient_3.all_foods(@user)).to eq(expected_3)
+      expect(@ingredient_4.all_foods(@user)).to eq(expected_4)
     end
   end
 end

93c330b1ef38cf556f610f71e8ceb108a7db852d
Jon Patterson <JonPatt92@gmail.com>
2020-01-08 11:37:24 -0700

    Add times_eaten and average_gut_feeling methods for ingredient model
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -9,12 +9,11 @@ class Ingredient < ApplicationRecord
   has_many :meal_ingredients, dependent: :destroy
   has_many :meals, through: :meal_ingredients
 
-  def times_eaten
+  def times_eaten(user)
+    meal_ingredients.joins(:meal).where(meals: {user_id: user.id}).count
   end
 
-  def avg_gut_feeling
-  end
-
-  def all_foods
+  def average_gut_feeling(user)
+    meals.where(user_id: user.id).average(:gut_feeling)
   end
 end

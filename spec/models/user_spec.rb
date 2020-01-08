6eb7bca390d433c37ab11fdbdea9660385d10f86
Ryan Hantak <rhantak@gmail.com>
2020-01-08 12:48:35 -0700

    Test best and worst ingredients methods and data methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,5 +41,66 @@ RSpec.describe User, type: :model do
       create_list(:meal, 8, user: user)
       expect(user.ratings_needed).to eq(-1)
     end
+
+    describe 'ingredient analysis' do
+      before(:each) do
+        @user = create(:user)
+        @meal_1 = create(:meal, user: @user, gut_feeling: -5)
+        @meal_2 = create(:meal, user: @user, gut_feeling: -4)
+        @meal_3 = create(:meal, user: @user, gut_feeling: -3)
+        @meal_4 = create(:meal, user: @user, gut_feeling: -2)
+        @meal_5 = create(:meal, user: @user, gut_feeling: -1)
+        @meal_6 = create(:meal, user: @user, gut_feeling: 0)
+        @meal_7 = create(:meal, user: @user, gut_feeling: 1)
+        @meal_8 = create(:meal, user: @user, gut_feeling: 2)
+        @meal_9 = create(:meal, user: @user, gut_feeling: 3)
+        @meal_10 = create(:meal, user: @user, gut_feeling: 4)
+        @meal_11 = create(:meal, user: @user, gut_feeling: 5)
+        @meal_12 = create(:meal, user: @user, gut_feeling: 2)
+
+        create(:meal_ingredient, meal: @meal_1)
+        create(:meal_ingredient, meal: @meal_2)
+        create(:meal_ingredient, meal: @meal_3)
+        create(:meal_ingredient, meal: @meal_4)
+        create(:meal_ingredient, meal: @meal_5)
+        create(:meal_ingredient, meal: @meal_6)
+        create(:meal_ingredient, meal: @meal_7)
+        create(:meal_ingredient, meal: @meal_8)
+        create(:meal_ingredient, meal: @meal_9)
+        create(:meal_ingredient, meal: @meal_10)
+        create(:meal_ingredient, meal: @meal_11)
+        create(:meal_ingredient, meal: @meal_12)
+      end
+
+      it 'best_ingredients' do
+        result = @user.best_ingredients
+
+        expect(result.length).to eq(6)
+        expect(result.first.avg_gut_feeling).to eq(5)
+        expect(result.last.avg_gut_feeling).to eq(1)
+      end
+
+      it 'best_ingredients_data' do
+        result = @user.best_ingredients_data
+
+        expect(result.count).to eq(6)
+        expect(result.first.keys).to eq([:name, :avg_gut_feeling])
+      end
+
+      it 'worst_ingredients' do
+        result = @user.worst_ingredients
+
+        expect(result.length).to eq(5)
+        expect(result.first.avg_gut_feeling).to eq(-5)
+        expect(result.last.avg_gut_feeling).to eq(-1)
+      end
+
+      it 'worst_ingredients_data' do
+        result = @user.worst_ingredients_data
+
+        expect(result.count).to eq(5)
+        expect(result.first.keys).to eq([:name, :avg_gut_feeling])
+      end
+    end
   end
 end

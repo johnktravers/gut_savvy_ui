22695d8dcce3cf69c0a3e4486342cfd544da8102
Christopher Kelling <cj.kelling88@gmail.com>
2020-01-09 12:24:27 -0700

    Change food create to use the new gut savvy service
    Co-Authored-By: John Travers <johnktravers@users.noreply.github.com>
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,43 +4,43 @@ class FoodsController < ApplicationController
   def new; end
 
   def create
-    unless food = Food.find_by(upc: params[:food][:upc])
-      service = FDCService.new
-      food_info = service.food_info(params[:food][:upc])
+    if food = Food.find_by(upc: params[:food][:upc])
+      session[:foods] << food.id
+      redirect_to new_dish_path
+    else
+      @service ||= GutSavvyService.new
+      food_info = @service.food_info(params[:food][:upc])
       if food_info && params[:food][:upc] != ''
         food_success(food_info)
       else
         food_error
       end
-    else
-      session[:foods] << food.id
-      redirect_to new_dish_path
     end
   end
 
   private
 
   def require_food_session
-    render file: "/public/404" if session[:foods].nil?
+    render file: '/public/404' if session[:foods].nil?
   end
 
   def create_food(food_info)
     Food.create(
-      name: food_info[:description],
-      brand: food_info[:brandOwner],
-      upc: food_info[:gtinUpc]
+      name: food_info[:name],
+      brand: food_info[:brand],
+      upc: food_info[:upc]
     )
   end
 
   def food_success(food_info)
     food = create_food(food_info)
-    food.create_ingredients(food_info)
-    session[:foods] << food.id.to_s
+    food.create_ingredients(food_info[:ingredients])
+    session[:foods] << food.id
     redirect_to new_dish_path
   end
 
   def food_error
-    flash[:error] = "Please enter a valid 12-digit UPC."
+    flash[:error] = 'Please enter a valid 12-digit UPC.'
     redirect_to new_food_path
   end
 end

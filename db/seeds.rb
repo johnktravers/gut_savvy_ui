dfeb0e79965dedfaa22060716578cdfc7cbfb888
Jon Patterson <JonPatt92@gmail.com>
2020-01-09 17:38:20 -0700

    Add meals through Jan 9th

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,9 @@ FoodIngredient.destroy_all
 Ingredient.destroy_all
 
 user = User.create!(
-  uid: '117045648135848876970',
-  name: 'John Travers',
-  email: 'john@example.com',
+  uid:   '104836038866663614181',
+  name:  'Rick Sanchez',
+  email: 'gutsavvy@gmail.com',
   token: ENV['GOOGLE_TOKEN']
 )
 
@@ -39,7 +39,7 @@ meal_hash = {
     }
   ],
 
-  'Dinner, 2019-12-22 01:01:31 UTC, 3' =>
+  'Dinner, 2019-12-21 01:01:31 UTC, 3' =>
   [
     { 'Bowties with Marinara' =>
       {
@@ -55,17 +55,17 @@ meal_hash = {
     }
   ],
 
-  'Breakfast, 2019-12-22 14:41:09 UTC, 0' =>
+  'Breakfast, 2019-12-22 14:41:09 UTC, 4' =>
   [
     { 'Raisin Bran with Almond Milk' =>
       {
         raisin_bran: '708820033429',
-        almond_milk: '025293001466'
+        almond_milk: '025293002043'
       }
     }
   ],
 
-  'Snack, 2019-12-22 19:34:25 UTC, -2' =>
+  'Snack, 2019-12-22 19:34:25 UTC, 0' =>
   [
     { 'Peanut Toffee Buzz Bar' =>
       {
@@ -74,7 +74,7 @@ meal_hash = {
     }
   ],
 
-  'Dinner, 2019-12-23 02:01:31 UTC, 4' =>
+  'Dinner, 2019-12-22 23:01:31 UTC, 3' =>
   [
     { 'Chicken Coconut Curry' =>
       {
@@ -85,17 +85,17 @@ meal_hash = {
     }
   ],
 
-  'Breakfast, 2019-12-23 14:20:17 UTC, 3' =>
+  'Breakfast, 2019-12-23 14:20:17 UTC, 4' =>
   [
     { 'Hot Cereal' =>
       {
         hot_cereal:  '072400060083',
-        almond_milk: '025293001466'
+        almond_milk: '025293002043'
       }
     }
   ],
 
-  'Lunch, 2019-12-23 19:34:25 UTC, 4' =>
+  'Lunch, 2019-12-23 19:34:25 UTC, 3' =>
   [
     { 'Turkey Sandwich' =>
       {
@@ -112,7 +112,7 @@ meal_hash = {
     }
   ],
 
-  'Snack, 2019-12-23 21:56:25 UTC, -4' =>
+  'Snack, 2019-12-23 21:56:25 UTC, -1' =>
   [
     { 'Chocolate Sandwich Cookie' =>
       {
@@ -131,6 +131,781 @@ meal_hash = {
         enchilada_sauce: '054791100098',
         black_beans:     '099482441630',
         pepper_jack:     '275148002838'
+      }
+    }
+  ],
+
+  'Healthy Breakfast, 2019-12-25 14:54:09 UTC, 5' =>
+  [
+    { 'Almond Milk Cereal' =>
+      {
+        oat_cereal:  '058449771036',
+        almond_milk: '025293002043'
+      }
+    },
+    { 'Granola' =>
+      {
+        granola_with_sugar: '857376002134',
+      }
+    }
+  ],
+
+  'Lunch, 2019-12-25 19:34:25 UTC, 5' =>
+  [
+    { 'Turkey Sandwich' =>
+      {
+        wheat_bread:  '025911000130',
+        turkey:       '011110005687',
+        mixed_greens: '041443112721',
+        mustard:      '011110865540'
+      }
+    },
+    { 'Orange Juice' =>
+      {
+        orange_juice: '041303026793'
+      }
+    }
+  ],
+
+  'Treat, 2019-12-25 20:30:25 UTC, -5' =>
+  [
+    { 'Lowfat Ice Cream' =>
+      {
+        ice_cream: '070640009930'
+      }
+    }
+  ],
+
+  'Soup Dinner, 2019-12-25 23:01:31 UTC, 4' =>
+  [
+    { 'Vegetable Soup' =>
+      {
+        vegetable_soup: '021140022110'
+      }
+    }
+  ],
+
+  'Sweet Breakfast, 2019-12-26 14:50:09 UTC, -5' =>
+  [
+    { 'Lucky Charms Cereal' =>
+      {
+        sweetened_milk: '815473010414',
+        lucky_charms_cereal: '042400069379'
+      }
+    }
+  ],
+
+  'Lunch, 2019-12-26 19:34:25 UTC, 4' =>
+  [
+    { 'Turkey Sandwich' =>
+      {
+        wheat_bread:  '025911000130',
+        turkey:       '011110005687',
+        mixed_greens: '041443112721',
+        mustard:      '011110865540'
+      }
+    },
+    { 'Apple Juice' =>
+      {
+        apple_juice: '842798100506'
+      }
+    }
+  ],
+
+  'Salad Dinner, 2019-12-26 23:21:31 UTC, 5' =>
+  [
+    { 'Aragula and Brocolli Salad' =>
+      {
+        brocolli_salad: '688267194115',
+        aragula:        '688267011559'
+      }
+    }
+  ],
+
+  'Breakfast, 2019-12-27 14:21:17 UTC, 3' =>
+  [
+    { 'Hot Cereal' =>
+      {
+        hot_cereal:       '072400060083',
+        cin_raisin_bagel: '075925401249'
+      }
+    }
+  ],
+
+  'Lunch, 2019-12-27 19:32:25 UTC, -1' =>
+  [
+    { 'Pizza' =>
+      {
+        pizza: '856497004034'
+      }
+    },
+    { 'Juice' =>
+      {
+        cranberry_juice: '725439975698'
+      }
+    }
+  ],
+
+  'Snack, 2019-12-27 20:56:25 UTC, -3' =>
+  [
+    { 'Toffee' =>
+      {
+        peanut_toffee_bar: '722252166029'
+      }
+    }
+  ],
+
+  'Dinner, 2019-12-27 23:01:31 UTC, -4' =>
+  [
+    { 'Lasagna' =>
+      {
+        lasagna: '711667000264',
+        cheese:  '073866210166'
+      }
+    }
+  ],
+
+  'Breakfast, 2019-12-28 14:21:17 UTC, 4' =>
+  [
+    { 'Granola and Cereal' =>
+      {
+        almond_milk:        '025293002043',
+        raisin_bran:        '708820033429',
+        granola_with_sugar: '857376002134'
+      }
+    }
+  ],
+
+  'Lunch, 2019-12-28 19:32:25 UTC, 2' =>
+  [
+    { 'Turkey Sandwich' =>
+      {
+        wheat_bread:  '025911000130',
+        turkey:       '011110005687',
+        mixed_greens: '041443112721',
+        mustard:      '011110865540'
+      }
+    },
+    { 'Juice' =>
+      {
+        orange_juice: '041303026793'
+      }
+    }
+  ],
+
+  'Snack, 2019-12-28 20:56:25 UTC, -3' =>
+  [
+    { 'Ice Cream' =>
+      {
+        ice_cream: '070640009930'
+      }
+    }
+  ],
+
+  'Dinner, 2019-12-28 23:01:31 UTC, 4' =>
+  [
+    { 'Salad' =>
+      {
+        aragula:    '688267011559',
+        choy_salad: '895203001394'
+      }
+    },
+    { 'Soup' =>
+      {
+        vegetable_soup: '021140022110'
+      }
+    }
+  ],
+
+  'Breakfast, 2019-12-29 14:21:17 UTC, -3' =>
+  [
+    { 'Yogurt and Cream Cheese Bagel' =>
+      {
+        greek_yogurt:     '818290013613',
+        cream_cheese:     '070852992280',
+        cin_raisin_bagel: '075925401249'
+      }
+    }
+  ],
+
+  'Lunch, 2019-12-29 19:32:25 UTC, 2' =>
+  [
+    { 'Chicken Curry' =>
+      {
+        coconut_milk: '737628011506',
+        curry_powder: '076114307557',
+        chicken:      '796853100065'
+      }
+    },
+    { 'Juice' =>
+      {
+        apple_juice: '842798100506'
+      }
+    }
+  ],
+
+  'Snack, 2019-12-29 20:56:25 UTC, -5' =>
+  [
+    { 'Ice Cream' =>
+      {
+        ice_cream: '070640009930'
+      }
+    }
+  ],
+
+  'Dinner, 2019-12-29 23:01:31 UTC, -2' =>
+  [
+    { 'Pizza' =>
+      {
+        pizza:  '856497004034',
+        cheese: '073866210166'
+      }
+    }
+  ],
+
+  'Breakfast, 2019-12-30 14:21:17 UTC, -4' =>
+  [
+    { 'Lucky Charms' =>
+      {
+        lucky_charms_cereal: '042400069379',
+        sweetened_milk:      '815473010414',
+        greek_yogurt:        '818290013613'
+      }
+    }
+  ],
+
+  'Lunch, 2019-12-30 19:32:25 UTC, 3' =>
+  [
+    { 'Turkey Sandwich' =>
+      {
+        wheat_bread:  '025911000130',
+        turkey:       '011110005687',
+        mixed_greens: '041443112721',
+        mustard:      '011110865540'
+      }
+    },
+    { 'Juice' =>
+      {
+        cranberry_juice: '725439975698'
+      }
+    }
+  ],
+
+  'Snack, 2019-12-30 20:56:25 UTC, -1' =>
+  [
+    { 'Cookie' =>
+      {
+        chocolate_cookie: '845777005984'
+      }
+    }
+  ],
+
+  'Dinner, 2019-12-30 23:01:31 UTC, 1' =>
+  [
+    { 'Pasta and Red Sauce' =>
+      {
+        farfalle:       '085239005392',
+        marinara_sauce: '041129077122',
+        olive_oil:      '078742058238',
+        garlic_bread:   '077890448410'
+      }
+    }
+  ],
+
+  'Breakfast, 2019-12-31 14:21:17 UTC, -4' =>
+  [
+    { 'Cream Cheese Bagel' =>
+      {
+        cream_cheese:     '070852992280',
+        cin_raisin_bagel: '075925401249',
+        greek_yogurt:     '818290013613'
+      }
+    }
+  ],
+
+  'Lunch, 2019-12-31 19:32:25 UTC, -1' =>
+  [
+    { 'Quesadilla' =>
+      {
+        wheat_tortilla:  '073731004197',
+        chicken:         '796853100065',
+        salsa:           '609207865858',
+        enchilada_sauce: '054791100098',
+        black_beans:     '099482441630',
+        pepper_jack:     '275148002838'
+      }
+    },
+    { 'Juice' =>
+      {
+        orange_juice: '041303026793'
+      }
+    }
+  ],
+
+  'Snack, 2019-12-31 20:56:25 UTC, -3' =>
+  [
+    { 'Ice Cream' =>
+      {
+        ice_cream: '070640009930'
+      }
+    }
+  ],
+
+  'Dinner, 2019-12-31 23:01:31 UTC, 4' =>
+  [
+    { 'Soup and Salad' =>
+      {
+        vegetable_soup: '021140022110',
+        brocolli_salad: '688267194115',
+        aragula:        '688267011559'
+      }
+    }
+  ],
+
+  'Breakfast, 2020-01-01 14:21:17 UTC, 3' =>
+  [
+    { 'Bagel and Jam' =>
+      {
+        raspberry_jam:    '640671889988',
+        cin_raisin_bagel: '075925401249',
+        orange_juice:     '041303026793'
+      }
+    }
+  ],
+
+  'Lunch, 2020-01-01 19:32:25 UTC, 3' =>
+  [
+    { 'Salmon Burger' =>
+      {
+        salmon_burger: '884853610995'
+      }
+    },
+    { 'Juice' =>
+      {
+        apple_juice: '842798100506'
+      }
+    }
+  ],
+
+  'Snack, 2020-01-01 20:56:25 UTC, -4' =>
+  [
+    { 'Cookie' =>
+      {
+        chocolate_cookie: '845777005984'
+      }
+    }
+  ],
+
+  'Dinner, 2020-01-01 23:01:31 UTC, -2' =>
+  [
+    { 'Lasagna' =>
+      {
+        lasagna: '711667000264'
+      }
+    }
+  ],
+
+  'Breakfast, 2020-01-02 14:21:17 UTC, 2' =>
+  [
+    { 'Cereal' =>
+      {
+        hot_cereal:  '072400060083',
+        almond_milk: '025293002043'
+      }
+    }
+  ],
+
+  'Lunch, 2020-01-02 19:32:25 UTC, -3' =>
+  [
+    { 'Pizza' =>
+      {
+        pizza:        '856497004034',
+        garlic_bread: '077890448410',
+        cheese:       '073866210166'
+      }
+    },
+    { 'Juice' =>
+      {
+        orange_juice: '041303026793'
+      }
+    }
+  ],
+
+  'Snack, 2020-01-02 20:56:25 UTC, -1' =>
+  [
+    { 'Ice Cream' =>
+      {
+        ice_cream: '070640009930'
+      }
+    }
+  ],
+
+  'Dinner, 2020-01-02 23:01:31 UTC, 5' =>
+  [
+    { 'Soup and Salad' =>
+      {
+        vegetable_soup: '021140022110',
+        brocolli_salad: '688267194115'
+      }
+    }
+  ],
+
+  'Breakfast, 2020-01-03 14:21:17 UTC, -4' =>
+  [
+    { 'Lucky Charms' =>
+      {
+        greek_yogurt:         '818290013613',
+        cream_cheese:         '070852992280',
+        cin_raisin_bagel:     '075925401249',
+        lucky_charms_cereal:  '042400069379',
+        sweetened_milk:       '815473010414',
+        cheese:               '073866210166'
+      }
+    }
+  ],
+
+  'Lunch, 2020-01-03 19:32:25 UTC, 4' =>
+  [
+    { 'Salad' =>
+      {
+        aragula:    '688267011559',
+        choy_salad: '895203001394'
+      }
+    },
+    { 'Juice' =>
+      {
+        cranberry_juice: '725439975698'
+      }
+    }
+  ],
+
+  'Snack, 2020-01-03 20:56:25 UTC, -1' =>
+  [
+    { 'Toffee' =>
+      {
+        peanut_toffee_bar: '722252166029'
+      }
+    }
+  ],
+
+  'Dinner, 2020-01-03 23:01:31 UTC, 3' =>
+  [
+    { 'Salmon Burger' =>
+      {
+        salmon_burger: '884853610995',
+        aragula:       '688267011559'
+      }
+    }
+  ],
+
+  'Breakfast, 2020-01-04 14:21:17 UTC, 1' =>
+  [
+    { 'Raisin Bran and Yogurt' =>
+      {
+        greek_yogurt:     '818290013613',
+        raspberry_jam:    '640671889988',
+        cin_raisin_bagel: '075925401249',
+        raisin_bran:      '708820033429',
+        almond_milk:      '025293002043'
+      }
+    }
+  ],
+
+  'Lunch, 2020-01-04 19:32:25 UTC, 1' =>
+  [
+    { 'Turkey Sandwich' =>
+      {
+        wheat_bread:  '025911000130',
+        turkey:       '011110005687',
+        mixed_greens: '041443112721',
+        mustard:      '011110865540',
+        pepper_jack:  '275148002838'
+      }
+    },
+    { 'Juice' =>
+      {
+        apple_juice: '842798100506'
+      }
+    }
+  ],
+
+  'Snack, 2020-01-04 20:56:25 UTC, -2' =>
+  [
+    { 'Cookie' =>
+      {
+        chocolate_cookie: '845777005984'
+      }
+    }
+  ],
+
+  'Dinner, 2020-01-04 23:01:31 UTC, 5' =>
+  [
+    { 'Soup and Salad' =>
+      {
+        vegetable_soup: '021140022110',
+        brocolli_salad: '688267194115',
+        aragula:        '688267011559',
+        choy_salad:     '895203001394'
+      }
+    }
+  ],
+
+  'Breakfast, 2020-01-05 14:21:17 UTC, 3' =>
+  [
+    { 'Raisin Bran' =>
+      {
+        raisin_bran: '708820033429',
+        almond_milk: '025293002043'
+      }
+    }
+  ],
+
+  'Lunch, 2020-01-05 19:32:25 UTC, 1' =>
+  [
+    { 'Turkey Sandwich' =>
+      {
+        wheat_bread:  '025911000130',
+        turkey:       '011110005687',
+        mixed_greens: '041443112721',
+        mustard:      '011110865540'
+      }
+    },
+    { 'Juice' =>
+      {
+        orange_juice: '041303026793'
+      }
+    }
+  ],
+
+  'Snack, 2020-01-05 20:56:25 UTC, 5' =>
+  [
+    { 'Ginger' =>
+      {
+        ginger: '074574108783'
+      }
+    }
+  ],
+
+  'Dinner, 2020-01-05 23:01:31 UTC, -4' =>
+  [
+    { 'Lasagna' =>
+      {
+        lasagna:     '711667000264',
+        cheese:      '073866210166',
+        pepper_jack: '275148002838'
+      }
+    }
+  ],
+
+  'Breakfast, 2020-01-06 14:21:17 UTC, -5' =>
+  [
+    { 'Yogurt and Cereal' =>
+      {
+        greek_yogurt:     '818290013613',
+        cream_cheese:     '070852992280',
+        cin_raisin_bagel: '075925401249',
+        sweetened_milk:   '815473010414'
+      }
+    }
+  ],
+
+  'Lunch, 2020-01-06 19:32:25 UTC, 3' =>
+  [
+    { 'Salmon Burger' =>
+      {
+        salmon_burger: '884853610995'
+      }
+    },
+    { 'Juice' =>
+      {
+        cranberry_juice: '725439975698'
+      }
+    }
+  ],
+
+  'Snack, 2020-01-06 20:56:25 UTC, 4' =>
+  [
+    { 'Ginger' =>
+      {
+        ginger: '074574108783'
+      }
+    }
+  ],
+
+  'Dinner, 2020-01-06 23:01:31 UTC, 3' =>
+  [
+    { 'Soup and Salad' =>
+      {
+        vegetable_soup: '021140022110',
+        brocolli_salad: '688267194115'
+      }
+    }
+  ],
+
+  'Breakfast, 2020-01-07 14:21:17 UTC, 4' =>
+  [
+    { 'Raisin Bran' =>
+      {
+        raisin_bran: '708820033429',
+        almond_milk: '025293002043'
+      }
+    }
+  ],
+
+  'Lunch, 2020-01-07 19:32:25 UTC, 3' =>
+  [
+    { 'Chicken Curry' =>
+      {
+        coconut_milk: '737628011506',
+        curry_powder: '076114307557',
+        chicken:      '796853100065'
+      }
+    },
+    { 'Juice' =>
+      {
+        cranberry_juice: '725439975698'
+      }
+    }
+  ],
+
+  'Snack, 2020-01-07 20:56:25 UTC, 4' =>
+  [
+    { 'Ginger' =>
+      {
+        ginger: '074574108783'
+      }
+    }
+  ],
+
+  'Dinner, 2020-01-07 23:01:31 UTC, 0' =>
+  [
+    { 'Pizza' =>
+      {
+        pizza:        '856497004034',
+        garlic_bread: '077890448410'
+      }
+    }
+  ],
+
+  'Breakfast, 2020-01-08 14:21:17 UTC, -4' =>
+  [
+    { 'Sweet Cereal' =>
+      {
+        lucky_charms_cereal: '042400069379',
+        sweetened_milk:      '815473010414'
+      }
+    }
+  ],
+
+  'Lunch, 2020-01-08 19:32:25 UTC, 4' =>
+  [
+    { 'Turkey Sandwich' =>
+      {
+        wheat_bread:  '025911000130',
+        turkey:       '011110005687',
+        mixed_greens: '041443112721',
+        mustard:      '011110865540'
+      }
+    },
+    { 'Juice' =>
+      {
+        cranberry_juice: '725439975698'
+      }
+    }
+  ],
+
+  'Snack, 2020-01-08 20:56:25 UTC, 5' =>
+  [
+    { 'Ginger' =>
+      {
+        ginger: '074574108783'
+      }
+    }
+  ],
+
+  'Dinner, 2020-01-08 23:01:31 UTC, 4' =>
+  [
+    { 'Salmon Burger' =>
+      {
+        salmon_burger: '884853610995',
+        aragula:       '688267011559'
+      }
+    }
+  ],
+
+  'Breakfast, 2020-01-09 14:21:17 UTC, 2' =>
+  [
+    { 'Hot Cereal' =>
+      {
+        raspberry_jam:    '640671889988',
+        cin_raisin_bagel: '075925401249',
+        hot_cereal:       '072400060083',
+        almond_milk:      '025293002043'
+      }
+    }
+  ],
+
+  'Lunch, 2020-01-09 19:32:25 UTC, 4' =>
+  [
+    { 'Salad' =>
+      {
+        aragula:    '688267011559',
+        choy_salad: '895203001394'
+      }
+    },
+    { 'Juice' =>
+      {
+        cranberry_juice: '725439975698'
+      }
+    }
+  ],
+
+  'Snack, 2020-01-09 20:56:25 UTC, 5' =>
+  [
+    { 'Ginger' =>
+      {
+        ginger: '074574108783'
+      }
+    }
+  ],
+
+  'Dinner, 2020-01-09 23:01:31 UTC, -1' =>
+  [
+    { 'Chicken Quesadilla' =>
+      {
+        wheat_tortilla:  '073731004197',
+        chicken:         '796853100065',
+        salsa:           '609207865858',
+        enchilada_sauce: '054791100098',
+        black_beans:     '099482441630',
+        pepper_jack:     '275148002838'
+      }
+    }
+  ],
+
+  'Breakfast, 2020-01-10 14:21:17 UTC, 3' =>
+  [
+    { 'Cereal and Bagel' =>
+      {
+        raspberry_jam:    '640671889988',
+        cin_raisin_bagel: '075925401249',
+        oat_cereal:       '058449771036',
+        almond_milk:      '025293002043'
+      }
+    }
+  ],
+
+  'Lunch, 2020-01-10 19:32:25 UTC, 2' =>
+  [
+    { 'Salmon Burger' =>
+      {
+        salmon_burger: '884853610995',
+        aragula:       '688267011559'
+      }
+    },
+    { 'Juice' =>
+      {
+        apple_juice: '842798100506'
       }
     }
   ]

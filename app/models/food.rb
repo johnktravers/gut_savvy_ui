598c0ce6ab8c7f9199b1bbcf7aedeb7224d6d5a0
Jon Patterson <JonPatt92@gmail.com>
2020-01-21 17:12:08 -0700

    Remove brand and upc length validations to allow a wider set of acceptable foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,10 +1,10 @@
 class Food < ApplicationRecord
-  validates_presence_of :name, :brand
+  validates_presence_of :name#, :brand
   validates :upc,
             presence: true,
             uniqueness: { case_sensitive: false },
-            numericality: true,
-            length: { is: 12 }
+            numericality: true
+            # length: { is: 12 }
 
   has_many :dish_foods, dependent: :destroy
   has_many :dishes, through: :dish_foods

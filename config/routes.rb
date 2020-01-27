33cc75c84510475c5e086b553f0708b620714b19
John Travers <johnk.travers@gmail.com>
2020-01-26 19:40:14 -0700

    Change help route to home route to link to help section of home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  get '/help',                        to: 'help#show'
+  get '/home',                        to: 'welcome#index'
   get '/dashboard',                   to: 'users#show'
 
   get '/results',                     to: 'results#index'

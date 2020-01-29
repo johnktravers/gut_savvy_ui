86f3faff687e3444d5194d52aff37d52c6cd7b38
Jon Patterson <JonPatt92@gmail.com>
2020-01-28 17:24:45 -0700

    Add [0] to formatted ingredients to reference array within array. Replace multiple gsub methods with single regex gsub

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 User.destroy_all
 Meal.destroy_all
 MealIngredient.destroy_all
@@ -911,7 +910,7 @@ def create_food_and_ingredients(upc, meal)
 end
 
 def process_ingredients(ingredient_list)
-  formatted_ingredients    = format_ingredients(ingredient_list)
+  formatted_ingredients    = format_ingredients(ingredient_list)[0]
   consolidated_ingredients = consolidate_ingredients(formatted_ingredients).uniq.compact
   validate_ingredients(consolidated_ingredients)
 end
@@ -920,10 +919,8 @@ def format_ingredients(ingredient_list)
   ingredient_list
     .gsub(/ \[.*?\]/, '')
     .gsub(/ \(.*?\)/, '')
+    .gsub(/\(\)\*/, '')
     .gsub('ORGANIC ', '')
-    .gsub(')', '')
-    .gsub('(', '')
-    .gsub('*', '')
     .gsub('100%', '')
     .split('.').first
     .split(', 2%').first
@@ -935,9 +932,9 @@ end
 def consolidate_ingredients(ingredients)
   ingredients.map! do |ingredient|
     if ingredient.include?("MILK")
-      ingredient.replace("MILK") unless not_dairy_milk?(ingredient)
+      ingredient.replace "MILK" unless not_dairy_milk?(ingredient)
     elsif ingredient.include?("SALT")
-      ingredient.replace("SALT")
+      ingredient.replace "SALT"
     else
       ingredient
     end
